0f7cef33220fb31a88c80f2b0bc0bd32421beb17
Yasser-Shehab-El-Dean <yasser@axlr8.tech>
2023-08-14 11:29:27 +0300

    Added Next Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import CustomSelect from "./components/CustomSelect";
 
 export default function Home() {
   return (
     <>
       <h1>Whiteboard</h1>
-      <CustomSelect />
     </>
   );
 }
